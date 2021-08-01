39ac8f95dccc6928960365f8a2695cb362b5de60
Jusley Souza <jusleysouza31@gmail.com>
2021-08-01 13:01:01 -0300

    Player Builder

---
+++ src/player/player.java
@@ -0,0 +1,13 @@
+package player;
+
+public class player {
+    public String name;
+    public int victories;
+    public String choice;
+    public int playerMove;
+
+    public player(String name){
+        this.name = name;
+        this.victories = 0;
+    }
+}
